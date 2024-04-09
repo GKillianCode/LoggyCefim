bc141823244b1c508411552a968fc953c5fac566
Killian Godet <godetkillian@yahoo.com>
2024-04-09 16:47:51 +0200

    Update Home Page + Create alert style component

--- LoggyCefim/MainWindow.xaml.cs
+++ LoggyCefim/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LoggyCefim.Pages;
 
 namespace LoggyCefim
 {
@@ -23,6 +24,8 @@ namespace LoggyCefim
         public MainWindow()
         {
             InitializeComponent();
+
+            _mainFrame.Navigate(new home());
         }
     }
 }
